0719cbc3bbcae030031765fdf946d6bbbc2c7fec
Ox52 <harsh04241@gmail.com>
2025-12-11 10:42:56 +0530

    ...

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,18 +1,15 @@
-// app/(auth)/register/page.tsx
 import { AuthForm } from "@/components/auth-form";
 import { getSession } from "@/lib/auth";
 import { redirect } from "next/navigation";
 
-export default async function RegisterPage() {
-  // Redirect authenticated users from register page
-  const session = getSession();
-  if (await session) {
+export default async function LoginPage() {
+  const session = await getSession();
+  if (session) {
     redirect("/");
   }
-
   return (
     <div>
-      <AuthForm type="register" />
+      <AuthForm type="login" />
     </div>
   );
 }
